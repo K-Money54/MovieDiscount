f562a51597f8fe65edfef72e2cac995ff6afeeb0
K-Money54 <71709451+K-Money54@users.noreply.github.com>
2020-10-05 14:08:24 -0400

    completed it

--- MovieDiscount.java
+++ MovieDiscount.java
@@ -3,24 +3,36 @@
  * Date: September 25, 2020
  * Purpose: to print the cost of an item
  */
-
+		
 import java.util.Scanner;
-
+		
 public class MovieDiscount {
     public static void main (String [] args) {
         // Create the input scanner
         Scanner input = new Scanner(System.in);
-
-        // Ask the user for the side lengths
+                  
+        double cost = 14.5;
+        
+        // Ask the user for their age
         System.err.print("How old are you? ");
         int age = input.nextInt();
+        
+        if(age < 13 || age >= 65) {
+        	cost = cost - 3;
+        	
+        } 
         
         
         // Ask the user if they have a coupon
         System.err.print("Do you have a coupon? [true/false] ");
+        boolean hasCoupon = input.nextBoolean();
         
-
+        if (hasCoupon) {
+        	cost = cost - 2;
+        }
+        
+        
         // Print the cost of a movie ticket
-        
+        System.out.println("Your ticket costs $" + cost);
     }
 }
